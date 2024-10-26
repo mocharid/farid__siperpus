4f499b7a66bb9924320ec275a9e411b5e9aace07
aslabtif-unsur <labtif.unsur@gmail.com>
2024-10-26 09:47:39 +0700

    Pertemuan ke-4

---
+++ database/seeders/BookshelfSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Bookshelf;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BookshelfSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //query builder 
+        DB::table('bookshelves')->insert([
+            [
+                'code'=>'RAK01',
+                'name'=> 'Informatika',
+            ],
+            [
+                'code'=> 'RAK02',
+                'name'=> 'Sipil',
+            ],
+            [
+                'code'=> 'GTA03',
+                'name'=> 'Filsafat',
+            ]
+        ]);
+
+        //ORM
+        Bookshelf::create([
+            'code'=> 'RAK03',
+            'name'=> 'Industri',
+        ]);
+    }
+}
